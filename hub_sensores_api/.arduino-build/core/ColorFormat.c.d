fb876d36b7e6d4003ff4ff19d5ede77900c753e0
Danilo Araujo <daniloaraujo0000@gmail.com>
2026-04-19 23:32:45 -0300

    chore: remove db and build files from tracking

--- hub_sensores_api/.arduino-build/core/ColorFormat.c.d
+++
@@ -1,3 +0,0 @@
-U:\build\sensor\sensor-temp\hub_sensores_api\.arduino-build\core\ColorFormat.c.o: \
- C:\Users\dbispo\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.7\cores\esp32\ColorFormat.c \
- C:\Users\dbispo\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.7\cores\esp32\ColorFormat.h
