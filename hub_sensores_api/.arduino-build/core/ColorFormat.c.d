b9813f72b9c80922cf3bab720b0b05caa68ff4ea
unknown <HTTP/pfsense.amevotu.corp.br@AMEVOTU.CORP.BR>
2026-05-12 10:14:14 -0300

    Ignore and remove .arduino-build from tracking

--- hub_sensores_api/.arduino-build/core/ColorFormat.c.d
+++
@@ -1,3 +0,0 @@
-C:\Users\danilo\Documents\build\sensor\ pre\sensor-temp\hub_sensores_api\.arduino-build\core\ColorFormat.c.o: \
- C:\Users\danilo\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.8\cores\esp32\ColorFormat.c \
- C:\Users\danilo\AppData\Local\Arduino15\packages\esp32\hardware\esp32\3.3.8\cores\esp32\ColorFormat.h
